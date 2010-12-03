bc6d57962a01db907023a5f98de3b554c58d346c
AllSeeingEye <nobodyhere@yandex.ru>
2010-12-03 22:49:57 +0300

    Keyboard shortcuts and respective options.

--- defaults/preferences/barlesque.js
+++ defaults/preferences/barlesque.js
@@ -1,3 +1,5 @@
 ﻿pref("extensions.barlesque.collapsed", false); /* true = addon bar is collapsed, false = it's not */
 pref("extensions.barlesque.mode",      false); /* true = stick to the left edge, false = to the right edge */
 pref("extensions.barlesque.persist",   false); /* remember the collapsed state of addon bar or not? */
+pref("extensions.barlesque.shorthide", false); /* allow a shortcut for quick hiding/showing the add-on bar or not? */
+pref("extensions.barlesque.shortmove", false); /* allow a shortcut for re-aligning the add-on bar or not? */
